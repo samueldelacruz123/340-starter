5f92ff6e6b71b18fae8f25399a46a9486e75b944
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2025-09-20 13:03:59 +0800

    Task two completed

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,12 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const Util = require("../utilities")
 
 // Classification pages
-router.get("/type/:classificationId", invController.buildByClassificationId)
+router.get("/type/:classificationId", Util.handleErrors(invController.buildByClassificationId))
 
 // Vehicle detail page
-router.get("/detail/:inventoryId", invController.buildByInventoryId)
+router.get("/detail/:inventoryId", Util.handleErrors(invController.buildByInventoryId))
 
 module.exports = router
