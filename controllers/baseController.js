94b54f4bf2808a234a0628391eb38a6ad21faf65
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2025-09-20 13:51:44 +0800

    Asignment 3 v1

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,23 @@
 const utilities = require("../utilities");
 const baseController = {};
 
+/* ************************
+ * Build Home view
+ ************************** */
 baseController.buildHome = async function(req, res){
     const nav = await utilities.getNav();
     res.render("index", {title: "Home", nav})
+}
+
+/* ************************
+ * Intentional Error Controller
+ ************************** */
+baseController.triggerError = async function(req, res, next) {
+  try {
+    throw new Error("This is an intentional 500 error for testing.")
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = baseController;
--- routes/static.js
+++ routes/static.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const baseController = require("../controllers/baseController");
 
 // Static Routes
-// Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
+
+// Intentional error route
+router.get("/trigger-error", baseController.triggerError);
 
 module.exports = router;
 
